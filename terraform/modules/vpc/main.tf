434d52844e33e9f85e7c048a7c251eb33b1bb491
Manish Gantyala <Manish05.devops@gmail.com>
2026-06-16 19:48:00 +0000

    PETPLAT-8: implement baseline security groups

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -81,3 +81,183 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+# ------------------------------
+# Security Groups
+# ------------------------------
+
+# EKS Cluster Security Group
+resource "aws_security_group" "eks_cluster" {
+  name        = "${var.project}-${var.environment}-eks-cluster-sg"
+  description = "EKS cluster control plane security group"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-eks-cluster-sg"
+    }
+  )
+}
+
+resource "aws_security_group_rule" "eks_cluster_ingress_nodes_443" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.eks_node.id
+  security_group_id        = aws_security_group.eks_cluster.id
+  description              = "API server access from EKS nodes"
+}
+
+resource "aws_security_group_rule" "eks_cluster_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_cluster.id
+  description       = "Allow all outbound traffic"
+}
+
+# EKS Node Security Group
+resource "aws_security_group" "eks_node" {
+  name        = "${var.project}-${var.environment}-eks-node-sg"
+  description = "EKS worker node security group"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-eks-node-sg"
+    }
+  )
+}
+
+resource "aws_security_group_rule" "eks_node_ingress_cluster_all" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.eks_cluster.id
+  security_group_id        = aws_security_group.eks_node.id
+  description              = "Allow all traffic from EKS cluster SG"
+}
+
+resource "aws_security_group_rule" "eks_node_ingress_self_all" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  self              = true
+  security_group_id = aws_security_group.eks_node.id
+  description       = "Allow inter-node communication"
+}
+
+resource "aws_security_group_rule" "eks_node_ingress_cluster_10250" {
+  type                     = "ingress"
+  from_port                = 10250
+  to_port                  = 10250
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.eks_cluster.id
+  security_group_id        = aws_security_group.eks_node.id
+  description              = "Kubelet API from cluster"
+}
+
+resource "aws_security_group_rule" "eks_node_ingress_alb_nodeport" {
+  type                     = "ingress"
+  from_port                = 30000
+  to_port                  = 32767
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.alb.id
+  security_group_id        = aws_security_group.eks_node.id
+  description              = "NodePort services from ALB"
+}
+
+resource "aws_security_group_rule" "eks_node_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_node.id
+  description       = "Allow all outbound traffic"
+}
+
+# RDS Security Group
+resource "aws_security_group" "rds" {
+  name        = "${var.project}-${var.environment}-rds-sg"
+  description = "RDS MySQL security group"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-rds-sg"
+    }
+  )
+}
+
+resource "aws_security_group_rule" "rds_ingress_nodes_3306" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.eks_node.id
+  security_group_id        = aws_security_group.rds.id
+  description              = "MySQL from EKS nodes only"
+}
+
+# ALB Security Group
+resource "aws_security_group" "alb" {
+  name        = "${var.project}-${var.environment}-alb-sg"
+  description = "ALB public-facing security group"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-alb-sg"
+    }
+  )
+}
+
+resource "aws_security_group_rule" "alb_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+  description       = "HTTP from internet"
+}
+
+resource "aws_security_group_rule" "alb_ingress_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+  description       = "HTTPS from internet"
+}
+
+resource "aws_security_group_rule" "alb_egress_nodes_nodeport" {
+  type                     = "egress"
+  from_port                = 30000
+  to_port                  = 32767
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.eks_node.id
+  security_group_id        = aws_security_group.alb.id
+  description              = "To EKS nodes for target groups"
+}
+
+resource "aws_security_group_rule" "alb_egress_nodes_8080" {
+  type                     = "egress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.eks_node.id
+  security_group_id        = aws_security_group.alb.id
+  description              = "Health checks to EKS nodes"
+}
