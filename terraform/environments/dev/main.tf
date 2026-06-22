4921f17d331625f8210c027d6120551a708aba11
Manish Gantyala <Manish05.devops@gmail.com>
2026-06-22 12:03:59 +0000

    PETPLAT-9: wire VPC module into dev environment

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -6,3 +6,14 @@ locals {
     ManagedBy   = "terraform"
   }
 }
+
+# PETPLAT-9: Wire VPC module into dev environment
+module "vpc" {
+  source              = "../../modules/vpc"
+  project             = var.project
+  environment         = var.environment
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  availability_zones  = ["eu-central-1a", "eu-central-1b"]
+  tags                = local.common_tags
+}
